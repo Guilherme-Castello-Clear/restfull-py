3063663d3cad8d666dd680d689cee80b6b276486
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-12 18:08:22 -0300

    Feat: Creating new coffees

--- main.py
+++ main.py
@@ -72,5 +72,36 @@ def get_all_cafes():
     all_cafes = result.scalars().all()
     return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
 
+
+@app.route("/search")
+def get_cafe_at_location():
+    query_location = request.args.get("loc")
+    result = db.session.execute(db.select(Cafe).where(Cafe.location == query_location))
+    all_cafes = result.scalars().all()
+    if all_cafes:
+        return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+    else:
+        return jsonify(error={"Not Found": "There's no coffee at your location."}), 404
+
+
+@app.route("/add", methods=["POST"])
+def create_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(res={"Success [200]:", "New coffee created"})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
